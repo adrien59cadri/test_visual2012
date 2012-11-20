85c24d93158bf1d0fc63f3778690a053c605c232
adrien courdavault <adrien.courdavault@arturia>
2012-11-20 19:10:40 +0100

    object

--- project/win/ConsoleApplication1/ConsoleApplication1.cpp
+++ project/win/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,6 +4,10 @@
 int main(int argc, char* argv[])
 {
     windows_helper::scanAudioEndpoints();
+
+    audio_system as;
+    for(int i=0;i<as.count();i++)
+        std::cout<<as.getDeviceInfoAt(i).mName.data()<<std::endl;
 	return 0;
 }
 
--- project/win/ConsoleApplication1/audio_core.h
+++ project/win/ConsoleApplication1/audio_core.h
@@ -11,9 +11,8 @@
 
 
 #include <iostream>
+#include <vector>
 
-class audio_device_info{
-};
 
 namespace windows_helper{
 #define WIN_SAFE_RELEASE(punk){ \
@@ -116,17 +115,112 @@ namespace windows_helper{
         WIN_SAFE_RELEASE( pEnumerator);
     }
 }
+enum class audio_direction{
+    eInput,
+    eOutput
+};
 
+class audio_device_info{
+public:
+    audio_device_info(const std::wstring & inName):mName(inName){}
+
+    std::wstring mName;
+private:
+    audio_device_info();
+};
+
+
+//depends on the API we use, for now only one API windows : MMDEVICE
 class audio_system{
-    audio_system(){
-    }
-    int getDevicesNb() const {
-    }
+public:
+    audio_system(){scan();}
+    int count()const{return mDevicesInfo.size();}
+    const audio_device_info& getDeviceInfoAt(int i) const{return mDevicesInfo.at(i);}
 
 private:
-    //see http://msdn.microsoft.com/en-us/library/windows/desktop/ms680582%28v=vs.85%29.aspx
-
-
     
+    //see http://msdn.microsoft.com/en-us/library/windows/desktop/ms680582%28v=vs.85%29.aspx
+    void scan(){
+        mInitialized = false;
+        mDevicesInfo.clear();
+        HRESULT hr= CoInitialize(nullptr);
+        //here hr could indicate that init is ok or that it was already initialized or a thread context changed ...
+
+        //create instance
+        IMMDeviceEnumerator * pEnumerator=nullptr;
+        const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
+        const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+        hr = CoCreateInstance(
+         CLSID_MMDeviceEnumerator, NULL,
+         CLSCTX_ALL, IID_IMMDeviceEnumerator,
+         (void**)&pEnumerator);
+
+        if(hr!=ERROR_SUCCESS|| pEnumerator==nullptr){
+            return ;
+        }
+
+        EDataFlow audio_direction = eAll;//or eCapture, or eRender, or eAll
+        DWORD device_state_mask = DEVICE_STATE_ACTIVE; // or DEVICE_STATE_ACTIVE or DEVICE_STATE_DISABLED or DEVICE_STATE_NOTPRESENT or DEVICE_STATE_UNPLUGGED or everything DEVICE_STATEMASK_ALL.
+        IMMDeviceCollection * pDeviceCollection=nullptr;
+        hr = pEnumerator->EnumAudioEndpoints(audio_direction,device_state_mask,&pDeviceCollection);
+        if(hr!=ERROR_SUCCESS || pDeviceCollection==nullptr){
+            return ;
+        }
+
+        unsigned count;
+        hr=pDeviceCollection->GetCount(&count);
+        if(hr!=ERROR_SUCCESS){
+            return ;
+        }
+
+        for(unsigned i=0;i<count;i++){
+            IMMDevice * pDevice=nullptr;
+            // Get pointer to endpoint number i.
+            hr = pDeviceCollection->Item(i, &pDevice);
+            if(hr!=ERROR_SUCCESS || pDevice == nullptr){
+                break ;
+            }
+                    // Get the endpoint ID string.
+            LPWSTR  device_id=nullptr;
+            hr = pDevice->GetId(&device_id);
+            if(hr!=ERROR_SUCCESS || device_id == nullptr){
+                break ;
+            }
+            IPropertyStore *pProps=nullptr;
+            hr = pDevice->OpenPropertyStore(
+                              STGM_READ, &pProps);
+            if(hr!=ERROR_SUCCESS || pProps == nullptr){
+                break ;
+            }
+
+            PROPVARIANT varName;
+            // Initialize container for property value.
+            PropVariantInit(&varName);
+
+            // Get the endpoint's friendly-name property.
+            hr = pProps->GetValue(PKEY_Device_FriendlyName, &varName);
+            if(hr!=ERROR_SUCCESS){
+                break ;
+            }
+
+
+            audio_device_info info(std::wstring(varName.pwszVal));
+            mDevicesInfo.push_back(info);
+
+            CoTaskMemFree(device_id);
+            PropVariantClear(&varName);
+            WIN_SAFE_RELEASE(pProps)
+            WIN_SAFE_RELEASE(pDevice);
+        }
+
+
+        //clean
+        WIN_SAFE_RELEASE( pDeviceCollection);
+        WIN_SAFE_RELEASE( pEnumerator);
+        mInitialized = true;
+    }
+
+    bool mInitialized;
+    std::vector<audio_device_info> mDevicesInfo;
 
 };
